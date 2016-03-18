0f37324b13923607064522e296282c156d9a90af
Nate Wallace <natewallace@gmail.com>
2016-03-18 10:04:24 -0400

    Fixed broken tests created by changes in the zone.js package.

--- src/tests/domSetup.js
+++ src/tests/domSetup.js
@@ -3,6 +3,9 @@ const jsdom = require('jsdom');
 // setup the simplest document possible
 const doc = jsdom.jsdom('<!doctype html><html><body id="page-body-content"><HostView>Loading...</HostView></div></body></html>');
 
+// load zone (for angular2) into the global object before defining window otherwise it will break
+require('zone.js/dist/zone');
+
 // get the window object out of the document
 const win = doc.defaultView;
 
@@ -20,6 +23,7 @@ for (const key in win) {
   global[key] = window[key];
 }
 
+// setup angular2 shims
 require('angular2/bundles/angular2-polyfills');
 global.Reflect = window.Reflect;
 window.requestAnimationFrame = function () {};
