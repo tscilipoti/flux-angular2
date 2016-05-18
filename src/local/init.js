3564378e8af5fb7143c867a191091a717df25f59
Nate Wallace <natewallace@gmail.com>
2016-05-18 07:59:52 -0400

    Update with init for environment

--- src/local/init.js
+++ src/local/init.js
@@ -1,6 +1,17 @@
 /**
- * Initialize the angular2 polyfills.
+ * Setup environment and initialize the angular2 polyfills.
  */
+if (typeof global === 'undefined' && typeof window !== 'undefined') {
+  window.global = window;
+  if (typeof process === 'undefined') {
+    window.process = {
+      env: {
+        NODE_ENV: 'production'
+      }
+    };
+  }
+}
+
 require('es6-shim');
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
